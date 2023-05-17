1199548b4abb147ecf6d6ebd406b4178db59599f
Aleksandr Provarenko <provar67@gmail.com>
2023-05-17 23:44:08 +0300

    Add Sign In Button

--- Bankey/LoginView.swift
+++ Bankey/LoginView.swift
@@ -47,6 +47,9 @@ extension LoginView {
         
         dividerView.translatesAutoresizingMaskIntoConstraints = false
         dividerView.backgroundColor = .secondarySystemFill
+        
+        layer.cornerRadius = 5
+        clipsToBounds = true
     }
     
     func layout() {
